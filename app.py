5cc056d0ba214d3d6299e4b9a59b0005944bde4b
ClaudioRapisardi01 <c.rapisardi@capecchispa.eu>
2025-11-10 16:38:28 +0100

    aa

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def ciao():
-    return '<h1>Ciao!</h1>'
+    return '<h1>Ciaoooo!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000, debug=True)
